1921fde02cf98e538ed648ab2c8d7363eb2eef32
thnikas <8nikas@gmail.com>
2026-09-22 22:26:29 +0200

    Adjust config and update packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["raw.githubusercontent.com"]
-    }
-}
-
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
